6b68b21bdb408b2e744cda1633d91605930c9fd0
Fabo xw <55964635+FabianMartinez1234567@users.noreply.github.com>
2020-07-12 14:37:01 -0300

    12/7

--- Programa/Programa.ino
+++ Programa/Programa.ino
@@ -1,9 +1,30 @@
-void setup() {
-  // put your setup code here, to run once:
+int Boton1=2;
+int Boton2=3;
+char ABC[27];//={"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "ñ", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
+void setup()
+{
+    Serial.begin(9600);
+    pinMode(2, INPUT);
+    pinMode(2, INPUT);
+    pinMode(4, OUTPUT);
+    pinMode(5, OUTPUT);
+}
+
+void loop()
+{
+    Serial.print("Hola");
+    botonera();  
 
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void botonera()
+{
+    if(Boton1==HIGH)
+    {
+        Serial.print("Hola");
+        digitalWrite(4, HIGH);
+    
+    }
+
 
 }
