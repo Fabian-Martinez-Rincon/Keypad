f812a9813368557b168f33979419b0df1f8bad9b
Fabo xw <55964635+FabianMartinez1234567@users.noreply.github.com>
2020-07-12 16:32:18 -0300

    12/7

--- Programa/Programa.ino
+++ Programa/Programa.ino
@@ -1,30 +1,44 @@
 int Boton1=2;
 int Boton2=3;
-char ABC[27];//={"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "ñ", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
+bool activarABC=false;
+char *myStrings[] = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "ñ", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
+                    };
+char *ABC[27];//={"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "ñ", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
 void setup()
 {
     Serial.begin(9600);
-    pinMode(2, INPUT);
-    pinMode(2, INPUT);
+    pinMode(Boton1, INPUT);
+    pinMode(Boton2, INPUT);
     pinMode(4, OUTPUT);
     pinMode(5, OUTPUT);
 }
 
 void loop()
 {
-    Serial.print("Hola");
+
+      
     botonera();  
 
 }
 
 void botonera()
 {
-    if(Boton1==HIGH)
+    int j=3;
+    if(digitalRead(Boton1)==HIGH)
     {
-        Serial.print("Hola");
+      
+        abc();
         digitalWrite(4, HIGH);
+        delay(500);
+        digitalWrite(4,LOW);
     
     }
 
-
 }
+void abc(){
+  for (int i = 0; i < 6; i++) {
+    Serial.println(myStrings[i]);
+    delay(500);
+  }
+  
+  }
