5e7c66f4ad1886eed38199ec82f507d51c34c0d5
Fabo xw <55964635+FabianMartinez1234567@users.noreply.github.com>
2020-07-13 18:25:46 -0300

    13/7

--- Programa/Programa.ino
+++ Programa/Programa.ino
@@ -1,9 +1,10 @@
 int SUBIR=2;
 int BAJAR=3;
+int tecla=0;
 int CONFIRMAR=4;
 bool activarABC=false;
 char *tecladoABC[] = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "ñ", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
-char opciones[20][19]= {{"dsa" },{"bolas"}};
+char opciones[20][19]= {{"" },{""}};
 void setup()
 {
     Serial.begin(9600);
@@ -16,7 +17,7 @@ void setup()
 
 void loop()
 {
-    CargarOpcion();
+//    CargarOpcion();
     
     botonera();  
 
@@ -24,22 +25,46 @@ void loop()
 
 void botonera()
 {
-    int j=3;
+   
     if(digitalRead(SUBIR)==HIGH)
     {
-      
-        abc();
-        digitalWrite(4, HIGH);
-        delay(500);
-        digitalWrite(4,LOW);
+        
     
     }
    if(digitalRead(BAJAR)==HIGH)
    {
       
+        
    }
+   if(digitalRead(CONFIRMAR)==HIGH)
+   {
+    
+    }
 
 }
+void subir()
+{
+tecla++;
+        Serial.println(tecladoABC[tecla]);
+        
+        digitalWrite(5, HIGH);
+        delay(200);
+        digitalWrite(5,LOW);
+}
+void bajar()
+{
+  tecla--;
+        Serial.println(tecladoABC[tecla]);
+        digitalWrite(6, HIGH);
+        delay(200);
+        digitalWrite(6,LOW);
+  
+  
+  }
+  void confirmar(){
+    
+    
+    }
 void abc(){
   for (int i = 0; i < 6; i++) {
     Serial.println(tecladoABC[i]);
